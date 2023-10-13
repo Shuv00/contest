f9a14d02e3670e5e8ce5d711a9bfaf0b818838ee
Shakil Ahamed Shuvo <100990764+Shuv00@users.noreply.github.com>
2023-10-13 22:40:52 +0600

    Update Problem_E.cpp

--- ICPC-Preli/Problem_E.cpp
+++ ICPC-Preli/Problem_E.cpp
@@ -4,40 +4,6 @@ using namespace std;
 #define no "NO"
 #define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define ll long long
-bool sortbysecond(const pair<ll,ll> &a,
-              const pair<ll,ll> &b)
-{
-    return (a.second < b.second);
-}
-void solve()
-{
-   ll n ;
-   cin >> n;
-   ll a[n], cou= 0, ans=0;
-   ll sum=1;
-   for(int i=0;i<n;i++){
-    cin >> a[i];
-   }
-   for(int i=0;i<n;i++){
-    if(a[i]==a[i+1]){
-        sum++;
-    }
-    else {
-       cou =(sum*(sum+1)) /2;
-      // cout << cou << endl;
-       ans  = ans+ cou;
-        //cout << ans << endl;
-       sum=1;
-    }
-   }
-   ll ns  = (n*(n+1))/2;
-   cout << ns - ans << endl;
-
-}
-
-
-
-
 int main(){
     fast;
     /**/int t;
